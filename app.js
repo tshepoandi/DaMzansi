8648add099122223d35d3a0913b76e9ec9c9a0ad
Tshepo <tshepo@localhost.localdomain>
2022-10-07 07:31:41 +0200

    add axios on windows instead

--- app.js
+++ app.js
@@ -65,3 +65,32 @@ app.listen(3000, (req, res) => {
 //       "text": "Do the dishes"
 //     }
 // }'
+
+// var axios = require('axios');
+// var data = JSON.stringify({
+//     "collection": "<COLLECTION_NAME>",
+//     "database": "<DATABASE_NAME>",
+//     "dataSource": "Cluster0",
+//     "projection": {
+//         "_id": 1
+//     }
+// });
+
+// var config = {
+//     method: 'post',
+//     url: 'https://data.mongodb-api.com/app/data-syjde/endpoint/data/v1/action/findOne',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Access-Control-Request-Headers': '*',
+//       'api-key': '<API_KEY>',
+//     },
+//     data: data
+// };
+
+// axios(config)
+//     .then(function (response) {
+//         console.log(JSON.stringify(response.data));
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
