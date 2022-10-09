babca2f5e453efe85842c6d3cf5497bbe11c8fdd
ndzalamabasa <ndzalamabasa@gmail.com>
2022-10-09 13:16:28 +0200

    added routed to serve the projects files

--- app.js
+++ app.js
@@ -1,56 +1,69 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
-let schema = mongoose.Schema
+let schema = mongoose.Schema;
 let url =
-  'mongodb+srv://tshepomashiloane:1234@cluster0.yuwyjxq.mongodb.net/?retryWrites=true&w=majority'
+  "mongodb+srv://tshepomashiloane:1234@cluster0.yuwyjxq.mongodb.net/?retryWrites=true&w=majority";
 // mongoapi = https://data.mongodb-api.com/app/data-syjde/endpoint/data/v1
 
 mongoose.connect(url, {
   useNewUrlParser: true,
-})
+});
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //create schema object
 
 let mySchema = mongoose.Schema({
-  username: 'String',
-  email: 'String',
-  password: 'String',
-})
+  username: "String",
+  email: "String",
+  password: "String",
+});
 
-let userData = mongoose.model('Users', mySchema)
+let userData = mongoose.model("Users", mySchema);
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-app.post('/', (req, res) => {
+app.get("/", (_, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/games", (_, res) => {
+  res.sendFile(__dirname + "/public/games.html");
+});
+
+app.get("/history", (_, res) => {
+  res.sendFile(__dirname + "/public/history.html");
+});
+
+app.get("/style.css", (_, res) => {
+  res.sendFile(__dirname + "/public/style.css");
+});
+
+app.post("/", (req, res) => {
   let info = {
     username: req.body.username,
     email: req.body.email,
     password: req.body.password[0],
     passwordConfirmation: req.body.password[0],
-  }
-  console.log(info)
-  let data = new userData(info)
+  };
+  console.log(info);
+  let data = new userData(info);
   data.save((error) => {
     if (error) {
-      console.log('error: ' + error)
+      console.log("error: " + error);
     } else {
-      console.log('Successfully saved')
+      console.log("Successfully saved");
     }
     // res.send('./public/games.html')
-  })
-})
+  });
+});
 
 app.listen(3000, (req, res) => {
-  console.log('listening on http://localhost:3000')
-})
+  console.log("listening on http://localhost:3000");
+});
 
 // curl --location --request POST 'https://data.mongodb-api.com/app/data-syjde/endpoint/data/v1/action/insertOne' \
 // --header 'Content-Type: application/json' \
