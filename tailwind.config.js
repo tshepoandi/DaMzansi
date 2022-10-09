fca0c1d0b6ac0b11f5870933cc6b524f5b5d1acd
ndzalamabasa <ndzalamabasa@gmail.com>
2022-10-09 12:56:17 +0200

    changed the path in which tailwind will be used

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
   },
   plugins: [],
 };
