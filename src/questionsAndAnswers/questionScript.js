b88464e33727f79d644a5611bebebb92cdd7401e
Tshepo <tshepo@localhost.localdomain>
2022-11-03 07:48:40 +0200

    questions

--- src/questionsAndAnswers/questionScript.js
+++ src/questionsAndAnswers/questionScript.js
@@ -69,18 +69,162 @@ const questions = {
     ],
   },
   4: {
-    question: '?',
+    question: `South Africa is often referred to as 'what sort of nation' which describes the country's multicultural diversity?`,
     options: [
       {
-        a: 466,
+        a: 'Democracy',
+        value: false,
+      },
+      { b: 'Free Nation', value: false },
+      { c: 'Rainbow Nation', value: true },
+      { d: 'none of the above', value: false },
+    ],
+  },
+  5: {
+    question: `South Africa accounts for 80% of the world's production of which precious metal?
+`,
+    options: [
+      {
+        a: 'Gold',
+        value: false,
+      },
+      { b: 'Silver', value: false },
+      { c: 'Diamond', value: false },
+      { d: 'Platinum', value: true },
+    ],
+  },
+  6: {
+    question: `Durban, the busiest port in South Africa, is on which ocean?`,
+    options: [
+      {
+        a: 'Atlantic Ocean',
+        value: false,
+      },
+      { b: 'Indian Ocean', value: true },
+      { c: 'Frank Ocean', value: false },
+      { d: 'none of the above', value: false },
+    ],
+  },
+  7: {
+    question: `When did Zulu people form a powerful state under the Shaka monarch`,
+    options: [
+      {
+        a: '1818',
         value: true,
       },
-      { b: 67, value: false },
-      { c: 27, value: false },
+      { b: '1942', value: false },
+      { c: '1961', value: false },
+      { d: 'none of the above', value: false },
+    ],
+  },
+  8: {
+    question: `Which writer was the first to be awarded the Booker Prize twice?`,
+    options: [
+      {
+        a: 'J.V Riebeck',
+        value: false,
+      },
+      { b: 'N.R Mandela', value: false },
+      { c: 'S.Biko', value: false },
+      { d: 'J.M Coetzee', value: false },
+    ],
+  },
+  9: {
+    question: `When did Zulu people form a powerful state under the Shaka monarch`,
+    options: [
+      {
+        a: '1818',
+        value: true,
+      },
+      { b: '1942', value: false },
+      { c: '1961', value: false },
+      { d: 'none of the above', value: false },
+    ],
+  },
+  10: {
+    question: `The First Boer War was fought from 1880 to 1881 between Britain and the South African Republic; which country was victorious?`,
+    options: [
+      {
+        a: 'British',
+        value: false,
+      },
+      { b: 'The South African', value: true },
+    ],
+  },
+  11: {
+    question: `Which rocky headland in Western Cape is the geographic southern tip of Africa?
+`,
+    options: [
+      {
+        a: 'Cape Town',
+        value: false,
+      },
+      { b: 'Madagascar', value: false },
+      { c: 'Khayelisha', value: false },
+      { d: 'Cape Agulhas', value: true },
+    ],
+  },
+  12: {
+    question: `The Kruger National Park contains the 'Big 5' of wild animals; which five animals make up this 'Big 5'?`,
+    options: [
+      {
+        a: 'Springboks, leopards, rhinos, elephants and buffalos',
+        value: false,
+      },
+      { b: 'Lions, leopards, rhinos, Springboks and buffalos', value: false },
+      { c: 'Tigers, leopards, rhinos, elephants and buffalos', value: false },
+      { d: 'Lions, cheetars, rhinos, elephants and buffalos', value: true },
+    ],
+  },
+  13: {
+    question: `True or False , South Africa is the only country in the world to have hosted the Football, Rugby and Cricket World Cups?`,
+    options: [
+      {
+        a: 'True',
+        value: true,
+      },
+      { b: 'False', value: false },
+    ],
+  },
+  14: {
+    question: `Which South African won the Nobel Peace Prize in 1984?`,
+    options: [
+      {
+        a: 'Whoopie Goldberg',
+        value: false,
+      },
+      { b: 'Desmond Tutu', value: true },
+      { c: 'Nelson Mandela', value: false },
+      { d: 'Steve Biko', value: false },
+    ],
+  },
+  15: {
+    question: `Who was the leader of South Africa from 1978 to 1989?`,
+    options: [
+      {
+        a: 'W F deKlerk',
+        value: false,
+      },
+      { b: 'Jan Van Portgier', value: false },
+      { c: 'P W Borter', value: true },
+      { d: 'P J Powers', value: false },
+    ],
+  },
+  16: {
+    question: `South African black population is divided into four major ethnic groups; Name Them`,
+    options: [
+      {
+        a: 'Nguni,Sotho,Shangaan-Tsonga,Venda',
+        value: true,
+      },
+      { b: 'Nguni, Tswana,Xhosa,Shangaan-Pedi', value: false },
+      { c: 'Nguni,Sotho, Shangaan-Venda, Tsonga', value: false },
       { d: 'none of the above', value: false },
     ],
   },
 }
+
+//  South Africa is divided into four major ethnic groups;
 
 function questionReceiver(questionForUser) {
   let correctAnswer = ''
